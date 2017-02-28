bb334bc94e26a3a70facfcb5c1a807624aaadb15
DanielKuehn87 <yourdekay@gmail.com>
2017-02-28 15:32:12 +0100

    Init commit bot and param definition
    param defs have to be completed.

---
+++ Bot.R
@@ -0,0 +1,57 @@
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(data.table, ggplot2, batchtools, mlr, ParamHelpers)
+
+source("Learner_Param_Def.R")
+
+# Create registry -------------------------------------------------------------------------------------------------
+unlink("./registry", recursive = TRUE)
+reg = makeExperimentRegistry(seed = 100L,
+                             packages = c("ParamHelpers", "cluster", "mlr"))
+#reg$cluster.functions = makeClusterFunctionsSocket(1)
+
+addProblem("PimaDiabetes", data = pid.task, reg = reg)
+
+runBot <- function(job, data, instance, mlr.lrn.name, predict.threshold, ...) {
+  mlr.task = data
+  mlr.res = makeResampleDesc("CV", iters = 10)
+  mlr.lrn = makeLearner(mlr.lrn.name,
+    predict.type = 'prob',
+    predict.threshold = predict.threshold)
+  
+  #Set hyperpars for learner
+  mlr.par.set = list(...)
+  lrn.pars = filterParams(getParamSet(mlr.lrn), tunable = TRUE)
+  mlr.par.set = mlr.par.set[names(mlr.par.set) %in% names(lrn.pars$pars)]
+  mlr.lrn = setHyperPars(mlr.lrn, par.vals = mlr.par.set)
+  
+  mlr.mod = resample(mlr.lrn,
+                      mlr.task, 
+                      resampling = mlr.res, 
+                      measures = getDefaultMeasure(mlr.task))
+  
+  return(list(mlr.mod = mlr.mod))
+}
+
+addAlgorithm("runBot", fun = runBot, reg = reg)
+algo.designs = list()
+algo.designs$runBot = DF.LEARNER.PARAM.SPECS
+
+addExperiments(algo.designs = algo.designs, reg = reg)
+summarizeExperiments(reg=reg)
+# submitJobs(ids = findNotDone()$job.id, reg=reg)
+# getJobStatus(1)
+# reduceResultsList(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Learner_Param_Def.R
@@ -0,0 +1,18 @@
+# Define tuning parameters for different learners
+DF.LEARNER.PARAM.SPECS 
+
+knn.params = makeParamSet(makeIntegerParam("k", lower = 1L, upper = 30L))
+knn.params = generateRandomDesign(n = 20L, knn.params)
+knn.params = unique(knn.params)
+knn.params$mlr.lrn.name = "classif.kknn"
+
+getParamSet(makeLearner("classif.svm"))
+svm.params = makeParamSet(makeIntegerParam("degree", lower = 2L, upper = 4L),
+  makeNumericParam("gamma", lower = 2^-10, upper = 2^10))
+
+
+
+
+
+
+
