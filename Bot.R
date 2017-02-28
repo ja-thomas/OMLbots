acafc0312cb9fdc5b5705d2044deeeb2a7b58ca8
Janek Thomas <janek.thomas@web.de>
2017-02-28 17:26:48 +0100

    .

--- Bot.R
+++ Bot.R
@@ -4,40 +4,39 @@ pacman::p_load(data.table, ggplot2, batchtools, mlr, ParamHelpers)
 source("Learner_Param_Def.R")
 
 # Create registry -------------------------------------------------------------------------------------------------
-unlink("./registry", recursive = TRUE)
+unlink("registry", recursive = TRUE)
 reg = makeExperimentRegistry(seed = 100L,
-                             packages = c("ParamHelpers", "cluster", "mlr"))
+                             packages = c("ParamHelpers", "cluster", "mlr", "OpenML"))
 #reg$cluster.functions = makeClusterFunctionsSocket(1)
 
-addProblem("PimaDiabetes", data = pid.task, reg = reg)
+addProblem("TASK", data = ids[1], reg = reg)
 
-runBot <- function(job, data, instance, mlr.lrn.name, predict.threshold, ...) {
-  mlr.task = data
-  mlr.res = makeResampleDesc("CV", iters = 10)
+runBot = function(job, data, instance, mlr.lrn.name, predict.threshold, ...) {
   mlr.lrn = makeLearner(mlr.lrn.name,
     predict.type = 'prob',
     predict.threshold = predict.threshold)
-  
+  data = getOMLTask(task.id = data)
   #Set hyperpars for learner
   mlr.par.set = list(...)
+  mlr.par.set = mlr.par.set[!vapply(mlr.par.set, is.na, logical(1))]
   lrn.pars = filterParams(getParamSet(mlr.lrn), tunable = TRUE)
   mlr.par.set = mlr.par.set[names(mlr.par.set) %in% names(lrn.pars$pars)]
   mlr.lrn = setHyperPars(mlr.lrn, par.vals = mlr.par.set)
   
-  mlr.mod = resample(mlr.lrn,
-                      mlr.task, 
-                      resampling = mlr.res, 
-                      measures = getDefaultMeasure(mlr.task))
-  
-  return(list(mlr.mod = mlr.mod))
+  measures = listMeasures(convertOMLTaskToMlr(data)$mlr.task, create = TRUE)
+
+  res = runTaskMlr(data, mlr.lrn, measures = measures)
+  uploadOMLRun(res, confirm.upload = FALSE)
+  res
 }
 
 addAlgorithm("runBot", fun = runBot, reg = reg)
 algo.designs = list()
-algo.designs$runBot = DF.LEARNER.PARAM.SPECS
+algo.designs$runBot = des
 
 addExperiments(algo.designs = algo.designs, reg = reg)
 summarizeExperiments(reg=reg)
+testJob(1)
 # submitJobs(ids = findNotDone()$job.id, reg=reg)
 # getJobStatus(1)
 # reduceResultsList(1)
--- Learner_Param_Def.R
+++ Learner_Param_Def.R
@@ -1,12 +1,30 @@
+library(BBmisc)
+  
+#OML tasks
+load("omlTaskIds.RData")
+
 # Define tuning parameters for different learners
-DF.LEARNER.PARAM.SPECS 
+#DF.LEARNER.PARAM.SPECS 
 
-knn.params = makeParamSet(makeIntegerParam("k", lower = 1L, upper = 30L))
-knn.params = generateRandomDesign(n = 20L, knn.params)
-knn.params = unique(knn.params)
-knn.params$mlr.lrn.name = "classif.kknn"
+#knn.params = makeParamSet(makeIntegerParam("k", lower = 1L, upper = 30L))
+#knn.params = generateRandomDesign(n = 20L, knn.params)
+#knn.params = unique(knn.params)
+#knn.params$mlr.lrn.name = "classif.kknn"
 
-getParamSet(makeLearner("classif.svm"))
-svm.params = makeParamSet(makeIntegerParam("degree", lower = 2L, upper = 4L),
-  makeNumericParam("gamma", lower = 2^-10, upper = 2^10))
+ps = makeParamSet(
+  makeDiscreteParam("kernel", values = c("linear", "polynomial", "radial")),
+  makeNumericParam("cost", lower = -10, upper = 10, trafo = function(x) 2^x),
+  makeNumericParam("gamma", lower = -10, upper = 10, trafo = function(x) 2^x, requires = quote(kernel == "radial")),
+  makeIntegerParam("degree", lower = 2, upper = 5, requires = quote(kernel == "polynomial")),
+  makeNumericParam("predict.threshold", lower = 0, upper = 1)
+)
+
+des = generateRandomDesign(100, ps)
+
+
+des$mlr.lrn.name = "classif.svm"
+des = convertDataFrameCols(des, factors.as.char = TRUE)
+
+#svm.params = makeParamSet(makeIntegerParam("degree", lower = 2L, upper = 4L),
+#  makeNumericParam("gamma", lower = 2^-10, upper = 2^10))
 
