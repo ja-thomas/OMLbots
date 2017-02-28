81388c47e8ea85525d652eb2aa4ed7c391e4fd85
Janek Thomas <janek.thomas@web.de>
2017-02-28 14:57:18 +0100

    exponential backoff

---
+++ exponentialBackOff.R
@@ -0,0 +1,30 @@
+exponentialBackOff = function(jobs, registry, start.resources = NULL, max.resources) {
+  
+  if (is.null(start.resources))
+    start.resources = registry$default.resources
+  
+  resourceTable = data.table(job.id = jobs, walltime = start.resources$walltime, memory = start.resources$memory)
+  
+  while (nrow(resourceTable) > 0) {
+    print(resourceTable)
+    
+    #drop jobs that are already on the limit and are still failing
+    resourceTable[walltime < max.resources$walltime & memory < max.resources$memory]
+    
+    if (nrow(resourceTable) == 0)
+      break
+    
+    for (i in seq_len(nrow(resourceTable)))
+      submitJobs(resourceTable[i, job.id], resources = list(walltime = resourceTable[i, walltime],
+        memory = resourceTable[i, memory], ntasks = start.resources$ntasks))
+    
+    memory.exceeded = grepLogs(pattern = "slurmstepd: Exceeded step memory limit at some point.")$job.id
+    time.exceeded = grepLogs(pattern = "slumstepd: [\\*]{3} JOB .+ DUE TO TIME LIMIT [\\*]{3}")$job.id
+    
+    # Drop successful jobs and increase walltime and memory for expired jobs
+    resourceTable = resourceTable[job.id %in% c(memory.exceeded, time.exceeded)]
+    resourceTable[job.id %in% memory.exceeded, memory := min(max.resources$memory, 2 * memory)]
+    resourceTable[job.id %in% time.exceeded, walltime := min(max.resources$walltime, 2 * walltime)]
+    
+  }
+}
---
+++ test_ExponentialBackOff.R
@@ -0,0 +1,15 @@
+source("exponentialBackOff.R")
+
+unlink("registry/", recursive = TRUE)
+
+reg = makeRegistry()
+
+f = function(i, j) {
+    Sys.sleep(i * 5)
+    x = rnorm(j * 50000)
+}
+
+x = expand.grid(i = 1:10, j = 1:10)
+
+batchMap(i = x$i, j = x$j, f)
+
