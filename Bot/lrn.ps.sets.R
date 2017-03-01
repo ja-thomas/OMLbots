09b4f08e5cab6770e7728df1e670afd915b2e5a0
Janek Thomas <janek.thomas@web.de>
2017-03-01 11:08:09 +0100

    use knn

--- Bot/lrn.ps.sets.R
+++ Bot/lrn.ps.sets.R
@@ -7,11 +7,11 @@ lrn.ps.sets = list(
       makeNumericParam("gamma", lower = -10, upper = 10, trafo = function(x) 2^x, requires = quote(kernel == "radial")),
       makeIntegerParam("degree", lower = 2, upper = 5, requires = quote(kernel == "polynomial"))
     )
-  # ),
-  # list(
-  #   lrn = makeLearner("classif.kknn", predict.type = "prob"),
-  #   ps = makeParamSet(
-  #     makeIntegerParam("k", lower = 1, upper = 30)
-  #   )
+  ),
+  list(
+    lrn = makeLearner("classif.kknn", predict.type = "prob"),
+    ps = makeParamSet(
+      makeIntegerParam("k", lower = 1, upper = 30)
+    )
   )
 )
