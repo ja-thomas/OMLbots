a6bb9c661fdfb305c59ea2b09cd460ba847d628a
Philipp Probst <philipp_probst@gmx.de>
2017-03-02 10:26:44 +0100

    addes some function for getting the results of the experiments

--- quickstart.R
+++ quickstart.R
@@ -2,3 +2,12 @@ library(devtools)
 load_all()
 
 runBot(batch.size = 1, lrn.ps.sets = lrn.ps.sets, upload = FALSE)
+
+system.time(overview <- getMlrRandomBotOverview("mlrRandomBotV1"))
+overview
+
+results = getMlrRandomBotResults(tag = "mlrRandomBotV1")
+results
+
+hypPars = getMlrRandomBotHyperpars(tag = "mlrRandomBotV1")
+hypPars
