480f2ba59edab3d98a5994d5964449e5c1376df0
Daniel Kuehn <Daniel.Kuehn@guycarp.com>
2017-04-05 18:29:48 +0200

    Should work for runtable. Hyperpar table is super slow...

--- R/getResults.R
+++ R/getResults.R
@@ -10,45 +10,54 @@ getMlrRandomBotOverview = function(tag = "mlrRandomBot") {
 
 # @param tag Name of the tag of the benchmark study.
 # @return [\code{data.frame}] Table with run.id, task.id, flow.id, flow.name, measure values.
-getRunTable = function(run.tag = "mlrRandomBot") {
-  numRuns = 50000 #FIXME: fix this once OpenML offers new solution
-  results = do.call("rbind", 
-    lapply(0:floor(numRuns/10000), function(i) {
-      return(listOMLRuns(tag = run.tag, limit = 10000, offset = (10000 * i) + 1))
-    })
-  )
+getRunTable = function(run.tag = "mlrRandomBot", excl.run.ids = NULL, local.db = NULL) {
+  if(is.null(local.db)){
+    numRuns = 20000 #FIXME: fix this once OpenML offers new solution
+    results = do.call("rbind", 
+      lapply(0:floor(numRuns/10000), function(i) {
+        return(listOMLRuns(tag = run.tag, limit = 10000, offset = (10000 * i) + 1))
+      })
+    )
+    
+    results = results[results$run.id %in% setdiff(results$run.id, excl.run.ids),]
+    
+    res = do.call("rbind", 
+      lapply(0:floor(nrow(results)/100), function(i) {
+        return(listOMLRunEvaluations(run.id = results$run.id[((100*i)+1):(100*(i+1))]))
+      })
+    )
+    
+    df = res %>%
+      gather(., key = "measure.name", value = "measure.value", -(run.id:upload.time), na.rm = TRUE) %>%
+      mutate(flow.version = c(stri_match_last(flow.name, regex = "[[:digit:]]+\\.*[[:digit:]]*")),
+        learner.name = stri_replace_last(flow.name, replacement = "", regex = "[([:digit:]]+\\.*[[:digit:]*)]"))
   
-  res = do.call("rbind", 
-    lapply(0:floor(nrow(results)/100), function(i) {
-      return(listOMLRunEvaluations(run.id = results$run.id[((100*i)+1):(100*(i+1))]))
-    })
-  )
-  
-  df = res %>%
-    gather(., key = "measure.name", value = "measure.value", -(run.id:upload.time), na.rm = TRUE) %>%
-    mutate(flow.version = c(stri_match_last(flow.name, regex = "[[:digit:]]+\\.*[[:digit:]]*")),
-      learner.name = stri_replace_last(flow.name, replacement = "", regex = "[([:digit:]]+\\.*[[:digit:]*)]"))
+  } else {
+    df = collect(tbl(local.db, sql("SELECT * FROM [run.table]")))
+  }
 
   return(df)
 }
 
 # @param tag Name of the tag of the benchmark study.
 # @return [\code{data.frame}] Table with run.id, hyperparameter name & value.
-getHyperparTable = function(run.tag = "mlrRandomBot", excl.run.id = NULL) {
-  numRuns = 50000 #FIXME: Fix this once OpenML offers different solution
+getHyperparTable = function(run.tag = "mlrRandomBot", excl.run.ids = NULL) {
+  numRuns = 20000 #FIXME: Fix this once OpenML offers different solution
   runs = do.call("rbind", 
     lapply(0:floor(numRuns/10000), function(i) {
       return(listOMLRuns(tag = run.tag, limit = 10000, offset = (10000 * i) + 1))
     })
   )
 
+  # FIXME: HORRIBLE performance
   res = lapply(runs$run.id, function(x){ #FIXME: Increase performance once OpenML offers solution
-    pars = getOMLRunParList(getOMLRun(x))
-    #FIXME: Just kill me now...
+    pars = tryCatch(getOMLRunParList(getOMLRun(x)), error = function(cond){return(NA)}) 
     if(length(pars) > 0){
-      pars = data.frame(do.call(rbind, lapply(pars, function(p) do.call(cbind, p))))
+      pars = ifelse(is.na(pars),
+        data.frame(name = "run_NA", value = NA, component = NA, stringsAsFactors = FALSE),
+        data.frame(do.call(rbind, lapply(pars, function(p) do.call(cbind, p))), stringsAsFactors = FALSE))
     } else {
-      pars = data.frame(name = "no_pars", value = NA, component = NA)
+      pars = data.frame(name = "no_pars", value = NA, component = NA, stringsAsFactors = FALSE)
     }
     
     pars$run.id = x
--- R/manageDatabase.R
+++ R/manageDatabase.R
@@ -4,26 +4,34 @@
 #' @return database
 initializeLocalDatabase = function(path = ".", overwrite = FALSE) {
   
-  path = paste0(path, "/mlrRandomBotDatabase.db")
+  db = paste0(path, "/mlrRandomBotDatabase.db")
   
-  if (file.exists(path) & overwrite) {
-    unlink(path)
+  if (file.exists(db) & overwrite) {
+    unlink(db, force = TRUE)
   } 
   
-  if (!file.exists(path)){
-    src = src_sqlite(path, create = TRUE)
+  if (!file.exists(db)){
+    src = src_sqlite(db, create = TRUE)
     
     run.table = data.frame(run.id = integer(0L), 
       task.id = integer(0L), 
+      setup.id = integer(0L),
       flow.id = integer(0L),
-      flow.name = character(0L), 
+      flow.name = character(0L),
+      flow.version = character(0L),
+      flow.source = character(0L),
+      learner.name = character(0L),
+      data.name = character(0L),
+      upload.time = character(0L),
       measure.name = character(0L), 
-      measure.value = numeric(0L))
+      measure.value = numeric(0L),
+      stringsAsFactors = FALSE)
     copy_to(src, run.table, temporary = FALSE)
     
     hyperpar.table = data.frame(run.id = integer(0L), 
       hyperpar.name = character(0L), 
-      hyperpar.value = character(0L))
+      hyperpar.value = character(0L),
+      stringsAsFactors = FALSE)
     copy_to(src, hyperpar.table, temporary = FALSE)
     
     meta.table = data.frame(task.id = integer(0L),
@@ -38,7 +46,8 @@ initializeLocalDatabase = function(path = ".", overwrite = FALSE) {
       number.of.instances.with.missing.values = integer(0L),
       number.of.missing.values = integer(0L),
       number.of.numeric.features = integer(0L),
-      number.of.symbolic.features = integer(0L))
+      number.of.symbolic.features = integer(0L),
+      stringsAsFactors = FALSE)
     copy_to(src, meta.table, temporary = FALSE)
     
   } else {
@@ -49,18 +58,20 @@ initializeLocalDatabase = function(path = ".", overwrite = FALSE) {
 }
 
 updateRunTable = function(run.tag, local.db){
-  df = getRunTable(run.tag = run.tag)
-  
+  run.ids <- collect(tbl(local.db, sql("SELECT DISTINCT [run.id] FROM [run.table]")))
+  df = getRunTable(run.tag = run.tag, excl.run.ids = run.ids)
+  db_insert_into(local.db$con, "run.table", df)
 }
 
 updateHyperparTable = function(run.tag, local.db){
+  run.ids <- collect(tbl(local.db, sql("SELECT DISTINCT [run.id] FROM [hyperpar.table]")))
   df = getHyperparTable(run.tag = run.tag)
-  
+  db_insert_into(local.db$con, "hyperpar.table", df)
 }
 
 updateMetaTable = function(task.tag, local.db){
   df = getMetaFeaturesTable(task.tag = task.tag)
-  
+  db_insert_into(local.db$con, "meta.table", df)
 }
 
 #' Check each tables of the local database for update requirement
@@ -72,17 +83,8 @@ updateLocalDatabase = function(path = ".", run.tag = "mlrRandomBot", task.tag = 
   
   local.db = initializeLocalDatabase(path = path)
   updateRunTable(run.tag = run.tag, local.db)
-  updateHyperparTable(run.tag = run.tag, local.db)
-  updateMetaTable(task.tag = task.tag, local.db)
-  
-  
-  
-  local.runs = tbl(db, "run.table")
-  
-  all.runs = listOMLRunEvaluations(tag = tag)
-  new.runs = setdiff(as.data.frame(local.runs)$run.id, all.runs$run.id)
-  
-  db_insert_into(db$con, table = "run.table", new.runs)
+  #updateHyperparTable(run.tag = run.tag, local.db)
+  #updateMetaTable(task.tag = task.tag, local.db)
 }
 
 
