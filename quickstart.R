8c9690887da699296fb81ff05ea6f79bf0e64ea3
PhilippPro <philipp_probst@gmx.de>
2017-04-06 10:37:41 +0200

    changed small things in the quickstart file

--- quickstart.R
+++ quickstart.R
@@ -4,30 +4,33 @@ load_all()
 # On LRZ
 # Serial cluster
 setOMLConfig(apikey = "34ebc7dff3057d8e224e5beac53aea0e")
-max.resources = list(walltime = 10000, memory = 2000)
+max.resources = list(walltime = 3600*6, memory = 2000)
 
-for(i in 1:1000) {
-  try(runBot(100, path = paste0("/naslx/projects/ua341/di49ruw/test", i), 
+for(i in 2:1000) {
+  try(runBot(500, path = paste0("/naslx/projects/ua341/di49ruw/test", i), 
     sample.learner.fun = sampleRandomLearner, sample.task.fun = sampleRandomTask, 
     sample.configuration.fun = sampleRandomConfiguration, max.resources = max.resources,  
     lrn.ps.sets = lrn.par.set, upload = TRUE, extra.tag = "botV1"))
 }
+
+
 # Locally
 for(i in 1:3){
-  runBot(10, path = "test", upload = TRUE)
+  runBot(3, lrn.ps.sets = lrn.par.set[5], path = "test", upload = TRUE)
 }
 
 overview = getMlrRandomBotOverview("botV1")
 print(overview)
 # Geht nicht bei zu großen Ergebnissen; stattdessen mit limit und listOMLRuns 
 tag = "mlrRandomBot"
-numRuns = 30000
+numRuns = 140000
 results = do.call("rbind", 
   lapply(0:floor(numRuns/10000), function(i) {
     return(listOMLRuns(tag = tag, limit = 10000, offset = (10000 * i) + 1))
   })
 )
 table(results$flow.id, results$task.id)
+table(results$uploader)
 
 res = do.call("rbind", 
   lapply(0:floor(nrow(results)/100), function(i) {
@@ -40,6 +43,9 @@ df = res %>%
       learner.name = stri_replace_last(flow.name, replacement = "", regex = "[([:digit:]]+\\.*[[:digit:]*)]"))
 as.data.frame.matrix(table(df$learner.name, df$data.name))
 
+
+for(i in 2:11)
+deleteOMLObject(results$run.id[i], object = "run")
 
 tbl.results = getMlrRandomBotResults("botV1")
 print(tbl.results)
