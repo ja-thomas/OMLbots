900e2195233b52af7ccb30cb14e666cd5b1eaaa4
DanielKuehn87 <yourdekay@gmail.com>
2017-06-23 23:36:34 +0200

    fix quickstart

--- quickstart2.R
+++ quickstart2.R
@@ -4,7 +4,7 @@ devtools::load_all()
 # On LRZ
 # Serial cluster
 setOMLConfig(apikey = "34ebc7dff3057d8e224e5beac53aea0e")
-lrn.par.set = testxgboost()
+lrn.par.set = getMultipleLearners ()
 i = sample(1:1e7, 1)
 bot.nr = 1
 
@@ -18,4 +18,4 @@ for(i in 1:20){
 data = lapply(1:5, function(x) listOMLRuns(tag = paste0("AzureBot", x)))
 dt = do.call(rbind, data)
 setDT(dt)
-dt[,.N,by=tags] #FIXME: xgboost seems to crash the bot on Azure. Might be some parallelization issue.
+dt[,.N,by=tags]
