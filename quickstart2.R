0b6e21b53f2154cc1ccd0a88dcca8c908737aefc
DanielKuehn87 <yourdekay@gmail.com>
2017-07-03 01:33:13 +0200

    fix overview

--- quickstart2.R
+++ quickstart2.R
@@ -15,7 +15,31 @@ for(i in 1:20){
          lrn.ps.sets = lrn.par.set, upload = TRUE, extra.tag = c("botV1", paste0("AzureBot", bot.nr)))
 }
 
-data = lapply(1:5, function(x) listOMLRuns(tag = paste0("AzureBot", x)))
+
+listFullOMLRuns = function(tag = NULL){
+  callOMLRuns = function(i) {
+    run.df = listOMLRuns(tag = tag, 
+      limit = max.limit,
+      offset = (max.limit * i) + 1)
+    return(run.df)
+  }
+  
+  df = tryCatch({listOMLRuns(tag = tag)},
+    error = function(err){
+      max.limit = 10000 #FIXME: fix this once OpenML offers new solution
+      nr.results = stringr::str_extract(err, "[0-9]{5,}")
+      message(paste0("Adjusting function to get ", nr.results, " results."))
+      
+      ls = lapply(seq(0, as.numeric(nr.results)/max.limit), function(i) callOMLRuns(i))
+      results = do.call("rbind", ls)
+      return(results)
+    })
+  
+  return(df)
+}
+
+
+data = lapply(1:25, function(x) listFullOMLRuns(tag = paste0("AzureBot", x)))
 dt = do.call(rbind, data)
 setDT(dt)
 dt[,.N,by=tags]
