9e59a3daf151e30656ca544f3813a51457ff4378
PhilippPro <philipp_probst@gmx.de>
2017-08-07 15:44:47 +0200

    added default values and runtime/scimark table

--- snapshot_database/database_extraction.R
+++ snapshot_database/database_extraction.R
@@ -1,6 +1,7 @@
 ############################################################################
 library(RMySQL)
 library(dplyr)
+library(tidyr)
 
 mydb = dbConnect(MySQL(), user = "root", dbname='openml')
 dbListTables(mydb)
@@ -8,15 +9,21 @@ dbListTables(mydb)
 ############################# evaluation_results ###########################
 
 # evaluation table
-evaluation =  dbSendQuery(mydb, "select * from evaluation")
+evaluation =  dbSendQuery(mydb, "select * from evaluation where function_id in (4, 59, 63)") # AUC, scimark, runtime
 evaluation = fetch(evaluation, n=-1)
 head(evaluation)
-# only AUC and runtime
+table(evaluation$function_id)
+# only AUC, scimark and runtime
 # id of math_function identifies the measure in evaluation (function_id)
-evaluation = evaluation[evaluation$function_id %in% c(4, 63),]
-# 6301439 evaluations
+# evaluation = evaluation[evaluation$function_id %in% c(4, 59, 63),] #  4, 59 und 63 werden benötigt
 colnames(evaluation)[1] = "rid"
 evaluation = evaluation[, c("rid", "function_id", "value")]
+
+# math_function table
+math_function =  dbSendQuery(mydb, "select * from math_function")
+math_function = fetch(math_function, n=-1)
+head(math_function)
+math_function$name
 
 # run table
 run =  dbSendQuery(mydb, "select * from run")
@@ -32,6 +39,8 @@ head(evaluation_results)
 # only runs of the open_ml bot
 evaluation_results = evaluation_results[evaluation_results$uploader == 2702,]
 head(evaluation_results)
+table(evaluation_results$function_id) 
+# Around 2886153 results
 
 ############################# meta_features ################################
 
@@ -78,8 +87,106 @@ head(hyperparameters)
 hyperparameters = hyperparameters[!(hyperparameters$name %in% c("nthread", "num.threads", "openml.kind", "openml.normal.kind", "openml.seed", "s", "verbose", "xval")), ]
 hyperparameters$fullName = gsub("\\(.*","",hyperparameters$fullName)
 table(hyperparameters$name)
+hyperparameters$input = NULL
+hyperparameters$input_id = NULL
 
+############################# Add default values ################################
+# rpart
+new_hypers_rpart = (hyperparameters[hyperparameters$fullName == "mlr.classif.rpart",])
+data_wide = spread(new_hypers_rpart, name, value)
+data_wide$maxdepth[is.na(data_wide$maxdepth)] = 30
+data_wide$minsplit[is.na(data_wide$minsplit)] = 20
+new_hypers_rpart = gather(data_wide, name, value = value, cp, maxdepth, minbucket, minsplit)
+hyperparameters = rbind(hyperparameters[hyperparameters$fullName != "mlr.classif.rpart",], new_hypers_rpart)
 
-# Add default values
+# kknn
+missing = evaluation_results[!(evaluation_results$setup %in% unique(hyperparameters$setup)), ]
+head(evaluation_results$setup)
+tabelle = table(hyperparameters[hyperparameters$fullName == "mlr.classif.kknn",]$value)
 
+missing = evaluation_results[!(evaluation_results$setup %in% unique(hyperparameters$setup)), ]
+missing_uni = sort(unique(missing$setup))
+algos = character(length(missing_uni))
+for(i in seq_along(missing_uni)) {
+  examine = missing[which(missing$setup == missing_uni[i])[1], ]
+  runi = getOMLRun(missing$rid[i])
+  print(runi$flow.name)
+  algos[i] = runi$flow.name
+  print(runi$parameter.setting)
+}
+# all the missing ones are kknn
+new_hypers_kknn = data.frame(setup = missing_uni, value = 7, fullName = "mlr.classif.kknn", name = "k")
+hyperparameters = rbind(hyperparameters, new_hypers_kknn)
+
+# svm
+new_hypers_svm = (hyperparameters[hyperparameters$fullName == "mlr.classif.svm",])
+data_wide = spread(new_hypers_svm, name, value)
+data_wide$kernel[is.na(data_wide$kernel)] = "radial"
+nas = is.na(data_wide[data_wide$kernel == "polynomial",]$degree)
+data_wide[data_wide$kernel == "polynomial",]$degree[nas] = 3
+new_hypers_svm = gather(data_wide, name, value = value, cost, degree, gamma, kernel)
+hyperparameters = rbind(hyperparameters[hyperparameters$fullName != "mlr.classif.svm",], new_hypers_svm)
+
+# ranger
+new_hypers_ranger = (hyperparameters[hyperparameters$fullName == "mlr.classif.ranger",])
+data_wide = spread(new_hypers_ranger, name, value)
+data_wide$num.trees[is.na(data_wide$num.trees)] = 500
+data_wide$replace[is.na(data_wide$replace)] = TRUE
+data_wide$respect.unordered.factors[is.na(data_wide$respect.unordered.factors)] = FALSE
+new_hypers_ranger = gather(data_wide, name, value = value, mtry, num.trees, replace, respect.unordered.factors, sample.fraction)
+hyperparameters = rbind(hyperparameters[hyperparameters$fullName != "mlr.classif.ranger",], new_hypers_ranger)
+
+#xgboost
+new_hypers_xgboost = (hyperparameters[hyperparameters$fullName == "mlr.classif.xgboost",])
+data_wide = spread(new_hypers_xgboost, name, value)
+data_wide$nrounds[is.na(data_wide$nrounds)] = 1
+data_wide$booster[is.na(data_wide$booster)] = "gbtree"
+nas = is.na(data_wide[data_wide$booster == "gbtree",]$max_depth)
+data_wide[data_wide$booster == "gbtree",]$max_depth[nas] = 6
+new_hypers_xgboost = gather(data_wide, name, value = value, alpha, booster, colsample_bylevel, colsample_bytree, eta, lambda, 
+  max_depth, min_child_weight, nrounds, subsample)
+hyperparameters = rbind(hyperparameters[hyperparameters$fullName != "mlr.classif.xgboost",], new_hypers_xgboost)
+
+# Reference table
+head(hyperparameters)
+new_hypers_ranger = hyperparameters[hyperparameters$fullName == "mlr.classif.ranger",]
+data_wide = spread(new_hypers_ranger, name, value)
+head(data_wide)
+data_wide = data_wide[data_wide$num.trees == 2000 & is.na(data_wide$sample.fraction),]
+reference = evaluation_results[evaluation_results$setup %in% data_wide$setup, ]
+evaluation_results = evaluation_results[!(evaluation_results$setup %in% data_wide$setup), ]
+hyperparameters = hyperparameters[!(hyperparameters$setup %in% data_wide$setup), ]
+
+# runtime
+runtime = evaluation_results[evaluation_results$function_id == 63, ]
+evaluation_results = evaluation_results[evaluation_results$function_id != 63, ]
+
+# scimark benchmark
+scimark =  evaluation_results[evaluation_results$function_id == 59, ]
+evaluation_results =  evaluation_results[evaluation_results$function_id != 59, ]
+
+delete_runtime = runtime$rid[!(runtime$rid %in% scimark$rid)]
+runtime = runtime[runtime$rid != 2414382, ]
+all(runtime$rid == scimark$rid)
+runtime = data.frame(runtime, scimark = scimark$value)
+rm(scimark)
+delete_columns = c("uploader", "setup", "task_id", "function_id", "data")
+runtime[, delete_columns] = NULL
+colnames(runtime)[2] = "runtime"
+delete_columns = c("uploader", "function_id")
+evaluation_results[, delete_columns] = NULL
+
+# Fit data to our current functions
+evaluation_results
+runtime
+meta_features
+hyperparameters
+reference
+
+save(evaluation_results, runtime, scimark, meta_features, hyperparameters, reference, file = "results.RData")
+tbl.results
+tbl.metaFeatures
+tbl.hypPars
+tbl.runTime
+tbl.resultsReference
 
