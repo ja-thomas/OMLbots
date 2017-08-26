09b63d0aa9d6adf46c0d667274e910c381e48e08
DanielKuehn87 <yourdekay@gmail.com>
2017-08-26 17:17:37 +0200

    Select tasks
    Only run tasks with less than 50000 results

--- quickstart2.R
+++ quickstart2.R
@@ -9,10 +9,10 @@ bot.nr = 1
 dt = fread("Azure_Overview.csv")
 dt[,GetDate := as.Date(GetDate, format = "%d/%m/%Y")]
 max.date = max(dt$GetDate)
-task.ignore = dt[GetDate == max.date,.(NrRuns = sum(NrRuns)), by = task.id][NrRuns > 100000, task.id]
+task.ignore = dt[GetDate == max.date,.(NrRuns = sum(NrRuns)), by = task.id][NrRuns > 50000, task.id]
 
 for(i in 1000:2500){
-  try(runBot(1000, path = paste0("test", i), 
+  try(runBot(1000, path = paste0("bot", i), 
          sample.learner.fun = sampleRandomLearner, sample.task.fun = sampleRandomAzureTask(bot.nr, task.ignore), 
          sample.configuration.fun = sampleRandomConfiguration,   
          lrn.ps.sets = lrn.par.set, upload = TRUE, extra.tag = c("botV1", paste0("AzureBot", bot.nr))))
